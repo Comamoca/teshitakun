8eb001f58166fc94384ec8707c28d6b0d4fdfe7d
HidemaruOwO <hidemal@outlook.jp>
2022-10-09 20:52:52 +0900

    :sparkles: コードの実行結果にTokenを含んでいたらメッセージを送信しないように変更 (src/commands/eval.ts)

--- src/commands/eval.ts
+++ src/commands/eval.ts
@@ -1,12 +1,17 @@
-import { ApplicationCommandOptionTypes, ApplicationCommandTypes, InteractionResponseTypes } from "../../deps.ts";
+import {
+  ApplicationCommandOptionTypes,
+  ApplicationCommandTypes,
+  InteractionResponseTypes,
+} from "../../deps.ts";
 import { createCommand } from "./mod.ts";
 import { $ } from "https://deno.land/x/dax@0.12.0/mod.ts";
 import { humanizeMilliseconds } from "../utils/helpers.ts";
+import { BOT_TOKEN } from "../../configs.ts";
 
 const denoEval = (cmd: string) => {
   const result = $`deno eval ${cmd}`.stdout("piped");
-  return result.stdout
-}
+  return result.stdout;
+};
 
 createCommand({
   name: "eval",
@@ -23,25 +28,45 @@ createCommand({
   scope: "Global",
   execute: async (bot, interaction) => {
     // メッセージからコンテンツの取得
-    const msgs = interaction.data?.options?.values()
+    const msgs = interaction.data?.options?.values();
 
     for (const msg of msgs) {
-      console.log(msg.value)
+      console.log(msg.value);
 
-      const bftime = Date.now()
+      const bftime = Date.now();
       const result = await $`deno eval ${msg.value}`.stdout("piped");
-      const exec_time = Date.now() - bftime
+      const exec_time = Date.now() - bftime;
 
-      await bot.helpers.sendInteractionResponse(
-        interaction.id,
-        interaction.token,
-        {
-          type: InteractionResponseTypes.ChannelMessageWithSource,
-          data: {
-            content: `🦕 Deno Eval\n\nYour Code:\`\`\`js\n${msg.value}\`\`\`\nResult: ${result.stdout}\nExecution time: ${exec_time}ms (${humanizeMilliseconds(exec_time)})`,
-          },
-        },
-      );
+      if (result.stdout.includes(BOT_TOKEN)) {
+        await bot.helpers.sendInteractionResponse(
+          interaction.id,
+          interaction.token,
+          {
+            type: InteractionResponseTypes.ChannelMessageWithSource,
+            data: {
+              content:
+                "⚠️ 結果にこのBotのTokenが含まれてたためメッセージの送信に失敗しました",
+            },
+          }
+        );
+      } else {
+        await bot.helpers.sendInteractionResponse(
+          interaction.id,
+          interaction.token,
+          {
+            type: InteractionResponseTypes.ChannelMessageWithSource,
+            data: {
+              content: `🦕 Deno Eval\n\nYour Code:\`\`\`js\n${
+                msg.value
+              }\`\`\`\nResult: ${
+                result.stdout
+              }\nExecution time: ${exec_time}ms (${humanizeMilliseconds(
+                exec_time
+              )})`,
+            },
+          }
+        );
+      }
     }
   },
 });
