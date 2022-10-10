007e8289203cf7f29c693104f84c36898941b786
HidemaruOwO <hidemal@outlook.jp>
2022-10-10 12:48:39 +0000

    :sparkles: customなconfigを読み込めるように変更 (config.json configs.ts)

--- configs.ts
+++ configs.ts
@@ -1,2 +1,5 @@
+import config from "./config.json" assert { type: "json" };
+
+export const customConfig = config;
 export const BOT_TOKEN = Deno.env.get("BOT_TOKEN") || "";
 export const BOT_ID = BigInt(atob(BOT_TOKEN.split(".")[0]));
